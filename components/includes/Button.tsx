a3d518ba115c639e6ee0be52670ff1781599e82a
Sabith <businessmansabith@gmail.com>
2024-05-07 15:54:34 +0530

    header

---
+++ components/includes/Button.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+type ButtonProps = {
+  type: "button" | "submit";
+  title: string;
+  variant: string;
+  margin: string;
+  icon?: string;
+};
+
+const Button = ({ type, title, variant, margin, icon }: ButtonProps) => {
+  return (
+    <button
+      type={type}
+      className={`${variant} ${margin} flexCenter gap-0 rounded-sm p-0`}>
+      {icon && <Image src={icon} alt={title} width={24} height={24} />}{" "}
+      <label className="text-black font-extrabold p-0">{title}</label>
+    </button>
+  );
+};
+
+export default Button;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,5 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,13 +7,43 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        gold: {
+          10: "#FFF62B",
+        },
+        gray: {
+          10: "#EEEEEE",
+          20: "#A2A2A2",
+          30: "#7B7B7B",
+          50: "#585858",
+          90: "#141414",
+        },
+        blue: {
+          10: "#356EDE",
+          20: "#1E5CFC",
+        },
+        green: {
+          90: "#000000",
+        },
+      },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "hero-pattern": "url('/lines.png')",
+      },
+      screens: {
+        xs: "400px",
+        "3xl": "1680px",
+        "4xl": "2200px",
+      },
+      maxWidth: {
+        "10xl": "1512px",
+      },
+      borderRadius: {
+        "5xl": "40px",
+      },
+      width: {
+        "128": "200rem",
       },
     },
   },
   plugins: [],
 };
-export default config;
